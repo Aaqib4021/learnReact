3e5bcfedd47af4a49a6b06af9e80df7aa5590fad
Aaqib Bashir <aqibnengroo5@gmail.com>
2024-07-27 15:04:50 +0530

    stage2

--- App.js
+++ App.js
@@ -2,9 +2,63 @@ import React from "react";
 import ReactDOM from 'react-dom/client';
 
 
-const heading =  React.createElement("h1",{},"this is an h1 from react!");
+// const heading =  React.createElement("h1",{},"this is an h1 from react!");
+// console.log(heading);
+
+//  const jsxheading = <h1>this is an h1 from jsx!</h1>//?single line
+// const jsxheading = (<h1>
+//     this is an h1 from jsx!
+//     </h1>)//?multiple lines () is used
+
+// console.log(jsxheading);
+
+// const HeadingComponent1 = ()=> {
+//     return <h1>this is an h1 from functional component! </h1>;//?same as below..
+// }
+
+// const HeadingComponent = () => (//?this is now a functional component 
+//     <h1>this is an h1 from functional component! </h1>
+// );
+
+// console.log(HeadingComponent);
+
+//?the aove code can be written inside the function not in arrow function 
+
+// const HeadingComponent = function (){
+//     return  <h1>this is an h1 from functional component! </h1>
+// }
+//?component compostion 
+//component inside another component
+// const Title = ()=>(
+//     <p>Hey this is my Title</p>
+// )
+
+
+// const HeadingComponent = ()=>(
+//     <div>
+//         <h1>This is my heading from component</h1>
+//         <Title/>
+//         <Title></Title>//?same as above
+//          {Title()}//?also same as it at last a function
+//     </div>
+// );
+
+
+
+//!what if element inside component 
+
+const title = <p>This is title</p>
+
+const HeadingComponent = ()=>(
+    <div>
+        <h1>This is my heading from component</h1>
+        {title}
+    </div>
+);
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(heading);
-//?this won't work because react is inside the node modules so we have to import that from there.. 
+root.render(<HeadingComponent/>);   
+//?this won't work because react is inside the node modules so we have to import that from there..  
--- README.md
+++ README.md
@@ -1,18 +1,37 @@
 # Learning React
 
-# parcel 
- - dev builds 
- -local server
- -HMR = hot module replacement 
- -caching -faster builds
- -bundling 
- -image optimization 
- -minification 
- -compressing
- -constent hashing
- -code splitting 
- -differential bundling
- -diagnostics 
- -Error handling
- -way to host ur app on https
- -Tree shaking -->remove unused code or minify it (some code which you or not using)
+# parcel
+
+- dev builds
+  -local server
+  -HMR = hot module replacement
+  -caching -faster builds
+  -bundling
+  -image optimization
+  -minification
+  -compressing
+  -constent hashing
+  -code splitting
+  -differential bundling
+  -diagnostics
+  -Error handling
+  -way to host ur app on https
+  -Tree shaking -->remove unused code or minify it (some code which you or not using)
+
+# now we do not want to give parcel index.html as for executing what we can do is update the package.json scripts and include start and another for both dev or production and give these commands there so that after we just need to give npm run start/build(name of that script) and prjct runs successfully . or we can use a shortcut npm start only for start (not for production).
+
+# React.createElement -->Object -->which is converted into the HTML Element with the help of render we do to render it on the page or we can say when we render it on the page until then it is an object..
+
+# JSX
+
+# babel
+
+# React components -->class & functional
+
+# functional -->start with Capital letter then ()={...}
+
+# To render functional component use <nameofthecomponent />
+
+# Component composition --> Component inside another component.
+
+# component using function keyword
